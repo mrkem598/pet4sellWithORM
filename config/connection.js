eadc8881c4f2243679ec1ed1b3aadd11075d660d
Moh <mohakemal9@gmail.com>
2017-05-08 02:13:22 -0400

    connection query finalized

--- config/connection.js
+++ config/connection.js
@@ -1,1 +1,17 @@
-﻿
+var mysql = require("mysql");
+var conneection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    database: "pets_db",
+    password: " "
+});
+connection.connect(function(err) {
+  if (err) {
+    console.log("error connecticting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+module.exports = connection;
+  
+                                                                                              
