7b2bb62d1ece3e2ace14a0f2efb14cc40bb3e113
Moh <mohakemal9@gmail.com>
2017-05-08 00:56:12 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
-﻿
+var orm = require("./config/orm.js");
+// Find all the pets ordering by the lowest price to the highest price.
+orm.selectAndOrder("animal_name", "pets", "price", "DESC");
+// Find a pet in the pets table by an animal_name of Rachel.
+orm.selectWhere("pets", "animal_name", "Rachel");
+// Find the buyer with the most pets.
+orm.selctWhoHasMost("buyer_name", "buyer_id", "buyers", "pets");
