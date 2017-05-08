82009090f1c94f543959105b7e3cb933dc78a2b8
Moh <mohakemal9@gmail.com>
2017-05-08 02:51:58 -0400

    Update orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,22 @@
-﻿
+// Requiring the exported module/file
+var connection = require("./connection.js");
+//Object Relational Mapping (ORM)
+var orm = {
+  selectWhere: function(tableInput, colToSearch, valOfCol) {
+    connection.query(queryString, [tableInput, colToSearch, valOfCol], function(err, result {
+     console.log(result);
+     });
+  },
+selectAndOrder: function(whatToSelect, table, orderCol, orderBy) {
+  var queryString = "SELECT ?? FROM ?? ORDER BY ? ?";
+  console.log(result);
+});
+},
+    findWhoHasMost: function(tableOneCal, tableTwoForeignKey, tableOne, tableTwo) {
+    var queryString = "SELECT ??, COUNT(??) AS count FROM ?? LEFT JOIN ?? ON ??.??= ??.ID GROUP BY ?? ORDER BY count DESC LIMIT 1";
+      connection.query(queryString, [tableOneCol, tableOneCol, tableOne, tableTwo, tableTwo, tableTwoForeignKey, tableOne, tableOneCol], function(err, result) {
+        console.log(result);
+      });
+    }
+};
+module.exports = orm;
